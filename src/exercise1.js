c9aa0d3e88ce8d8cc87ec68c1417e078acad2158
GALBY <william.galby@cgi.com>
2017-04-21 09:53:21 +0200

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -5,6 +5,13 @@ console.log(chalk.cyan('\nEXERCISE 1 LOADED\n'));
 const zip = (input) => {
 
   // Your future job begins here ...
+  return input.reduce(function(acc1, value1) {
+    value1.reduce(function(acc2, value2) {
+      return acc2.push(value2);
+    }, []);
+    return acc1.push(value1);
+  }, []);
+
 }
 
 module.exports = {
