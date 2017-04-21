d7ca89726dbd9765328d400dc37faa021329bfad
GALBY <william.galby@cgi.com>
2017-04-21 10:11:09 +0200

    Exercise 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -6,8 +6,7 @@ describe('GIVEN function reverseYolo', function() {
   it('WHEN my mystery input THEN my wonderful output', function() {
 
     // Your future job begins here ...
-    let whereIsMyMind = '???';
-
+    let whereIsMyMind = 'GKNORT33000112247';
     expect(reverseYolo(whereIsMyMind)).to.eql([ 'T', 'R', 'O', 'N', 'K', 'G', '3', '3', '0', 7, 4, 2, 2, 1, 1, 0, 0 ]);
   });
 
